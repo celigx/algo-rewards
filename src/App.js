e25e115e9074bd0ad86a8260034154a8ad4253dd
Celig <github@cela.anonaddy.com>
2021-12-30 23:15:51 +0100

    Add title

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.sass';
 function App() {
   return (
     <div className="app">
+
+      <div className="titleContainer">
+        <h1 className='title'>Governance Rewards Calculator</h1>
+      </div>
+      
     </div>
   );
 }
