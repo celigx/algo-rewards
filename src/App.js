004a3dea852ec2644bfe8fb3ee211ed497e1fa40
Celig <github@cela.anonaddy.com>
2021-12-30 23:23:25 +0100

    Add input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import './App.sass';
 
 function App() {
+  const [input, setInput] = useState(0)
+
+  // Update input on change
+  const inputValue = (e) => {
+    setInput(e.target.value)
+  }
+
   return (
     <div className="app">
 
       <div className="titleContainer">
         <h1 className='title'>Governance Rewards Calculator</h1>
       </div>
-      
+
+      <div className="container">
+        <div className="inputContainer">
+          <p className='title'>Enter your ALGO amount</p>
+          <input type="number" placeholder='Enter ammount' className='input' onChange={inputValue} />
+        </div>
+      </div>
+
     </div>
   );
 }
