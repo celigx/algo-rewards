4867e23febd781e3ef398fc8700529ae435c197c
Celig <github@cela.anonaddy.com>
2021-12-30 23:28:53 +0100

    Add fetch API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.sass';
 
 function App() {
+  const [committedStake, setCommittedStake] = useState(0)
+  const [rewardPoolAmount, setRewardPoolAmount] = useState(0)
   const [input, setInput] = useState(0)
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = () => {
+    fetch("https://governance.algorand.foundation/api/periods/governance-period-1/")
+    .then(response => response.json())
+    .then(response => {
+      setCommittedStake(response.total_committed_stake)
+      setRewardPoolAmount(response.algo_amount_in_reward_pool)
+    });
+  }
 
   // Update input on change
   const inputValue = (e) => {
