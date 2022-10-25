1aad658a7053f9a2b5c511faceeda13fefb9bf1a
celigx <l.celigx@gmail.com>
2022-10-25 23:03:55 +0200

    Add governance period #5

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { useEffect, useState } from 'react';
-import './App.sass';
-import GovernanceData from './components/GovernanceData';
-import GovernancePeriod from './components/GovernancePeriod';
-import GovernancePeriodModal from './components/GovernancePeriodModal';
-import Output from './components/OutputData';
-import { stakingAPY } from './helper/stakingAPY';
+import { useEffect, useState } from "react";
+import "./App.sass";
+import GovernanceData from "./components/GovernanceData";
+import GovernancePeriod from "./components/GovernancePeriod";
+import GovernancePeriodModal from "./components/GovernancePeriodModal";
+import Output from "./components/OutputData";
+import { stakingAPY } from "./helper/stakingAPY";
 
 function App() {
-  const [committedStake, setCommittedStake] = useState(0)
-  const [rewardPoolAmount, setRewardPoolAmount] = useState(0)
-  const [input, setInput] = useState(0)
-  const [governanceAPR, setGovernanceAPR] = useState(0)
-  const [governanceRewards, setGovernanceRewards] = useState(0)
-  const [stakingRewards, setStakingRewards] = useState(0)
-  const [totalRewards, setTotalRewards] = useState(0)
-  const [totalAlgo, setTotalAlgo] = useState(0)
-  const [governancePeriod, setGovernancePeriod] = useState(4)
-  const [governors, setGovernors] = useState(0)
+  const [committedStake, setCommittedStake] = useState(0);
+  const [rewardPoolAmount, setRewardPoolAmount] = useState(0);
+  const [input, setInput] = useState(0);
+  const [governanceAPR, setGovernanceAPR] = useState(0);
+  const [governanceRewards, setGovernanceRewards] = useState(0);
+  const [stakingRewards, setStakingRewards] = useState(0);
+  const [totalRewards, setTotalRewards] = useState(0);
+  const [totalAlgo, setTotalAlgo] = useState(0);
+  const [governancePeriod, setGovernancePeriod] = useState(5);
+  const [governors, setGovernors] = useState(0);
 
   const [governanceDateTime, setGovernanceDateTime] = useState({
     votingStart: 0,
@@ -24,100 +24,98 @@ function App() {
     periodStart: 0,
     registrationEnd: 0,
     periodEnd: 0,
-  })
+  });
 
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    getData()
-    calculateGovernanceAPR()
-    calculateGovernanceRewards()
-    calculateStakingRewards()
-    calculateTotalRewards()
-    calculateTotalAlgo()
+    getData();
+    calculateGovernanceAPR();
+    calculateGovernanceRewards();
+    calculateStakingRewards();
+    calculateTotalRewards();
+    calculateTotalAlgo();
 
-    // Hide scrollbar when modal is displayed 
-    showModal ? document.body.classList.add('hideScrollbar') : document.body.classList.remove('hideScrollbar')
-  }, [input, committedStake, rewardPoolAmount, governanceAPR, governanceRewards, stakingRewards,totalRewards, totalAlgo, governancePeriod, showModal])
+    // Hide scrollbar when modal is displayed
+    showModal ? document.body.classList.add("hideScrollbar") : document.body.classList.remove("hideScrollbar");
+  }, [input, committedStake, rewardPoolAmount, governanceAPR, governanceRewards, stakingRewards, totalRewards, totalAlgo, governancePeriod, showModal]);
 
   const getData = () => {
     fetch(`https://governance.algorand.foundation/api/periods/governance-period-${governancePeriod}/`)
-    .then(response => response.json())
-    .then(response => {
-      setCommittedStake(response.total_committed_stake)
-      setRewardPoolAmount(response.algo_amount_in_reward_pool)
-      setGovernors(response.governor_count)
-      setGovernanceDateTime(prevState => ({
-        ...prevState,
-        votingStart: response.voting_sessions[0] === undefined ? undefined : response.voting_sessions[0].voting_start_datetime,
-        votingEnd: response.voting_sessions[0] === undefined ? undefined : response.voting_sessions[0].voting_end_datetime,
-        periodStart: response.start_datetime,
-        registrationEnd: response.registration_end_datetime,
-        periodEnd: response.end_datetime
-      }))
-    });
-  }
+      .then((response) => response.json())
+      .then((response) => {
+        setCommittedStake(response.total_committed_stake);
+        setRewardPoolAmount(response.algo_amount_in_reward_pool);
+        setGovernors(response.governor_count);
+        setGovernanceDateTime((prevState) => ({
+          ...prevState,
+          votingStart: response.voting_sessions[0] === undefined ? undefined : response.voting_sessions[0].voting_start_datetime,
+          votingEnd: response.voting_sessions[0] === undefined ? undefined : response.voting_sessions[0].voting_end_datetime,
+          periodStart: response.start_datetime,
+          registrationEnd: response.registration_end_datetime,
+          periodEnd: response.end_datetime,
+        }));
+      });
+  };
 
   // Update input on change
   const inputValue = (e) => {
-    setInput(e.target.value)
-  }
+    setInput(e.target.value);
+  };
 
   const calculateGovernanceAPR = () => {
-    const governorCount = committedStake / 1000000
-    const rewardPool = rewardPoolAmount / 1000000
-    const APR = Number((rewardPool / governorCount) * 100 * 4)
-    
-    setGovernanceAPR(APR)
-  }
+    const governorCount = committedStake / 1000000;
+    const rewardPool = rewardPoolAmount / 1000000;
+    const APR = Number((rewardPool / governorCount) * 100 * 4);
+
+    setGovernanceAPR(APR);
+  };
 
   const calculateGovernanceRewards = () => {
-    const rewards = (input * governanceAPR / 100) / 12 * 3
-    
-    setGovernanceRewards(rewards)
-  }
+    const rewards = ((input * governanceAPR) / 100 / 12) * 3;
+
+    setGovernanceRewards(rewards);
+  };
 
   const calculateStakingRewards = () => {
-    const rewards = ((input * stakingAPY[governancePeriod]) / 100) / 4
+    const rewards = (input * stakingAPY[governancePeriod]) / 100 / 4;
 
-    setStakingRewards(rewards)
-  }
+    setStakingRewards(rewards);
+  };
 
   const calculateTotalRewards = () => {
-    const rewards = Number(governanceRewards) + stakingRewards
+    const rewards = Number(governanceRewards) + stakingRewards;
 
-    setTotalRewards(rewards)
-  }
+    setTotalRewards(rewards);
+  };
 
   const calculateTotalAlgo = () => {
-    const total = totalRewards + parseInt(input)
+    const total = totalRewards + parseInt(input);
 
-    setTotalAlgo(total)
-  }
+    setTotalAlgo(total);
+  };
 
   return (
     <div className="app">
-
       <div className="titleContainer">
-        <h1 className='title'>Governance Rewards Calculator</h1>
+        <h1 className="title">Governance Rewards Calculator</h1>
       </div>
 
       <GovernanceData committedStake={committedStake} rewardPoolAmount={rewardPoolAmount} governors={governors} />
 
       <div className="container">
         <div className="left">
-
           <div className="inputContainer">
-            <p className='title'>Enter your ALGO amount</p>
-            <input type="number" placeholder='Enter ammount' className='input' onChange={inputValue} />
+            <p className="title">Enter your ALGO amount</p>
+            <input type="number" placeholder="Enter ammount" className="input" onChange={inputValue} />
           </div>
 
-          <Output id="one" text='Governance APR' number={`${governanceAPR.toFixed(2)} %`} />
-          <Output id="two" text='Governance rewards' number={governanceRewards.toFixed(2)} />
-          <Output id="three" text='Staking APY' number={`${stakingAPY[governancePeriod]} %`} />
+          <Output id="one" text="Governance APR" number={`${governanceAPR.toFixed(2)} %`} />
+          <Output id="two" text="Governance rewards" number={governanceRewards.toFixed(2)} />
+          <Output id="three" text="Staking APY" number={`${stakingAPY[governancePeriod]} %`} />
           <Output id="four" text="Staking Rewards" number={stakingRewards.toFixed(2)} />
           <Output id="five" text="Total Rewards" number={totalRewards.toFixed(2)} />
-          <Output id="six" text="Total Algo After Period" number={input === '' ? (0).toFixed(2) : totalAlgo.toFixed(2)} />
+          <Output id="six" text="Total Algo After Period" number={input === "" ? (0).toFixed(2) : totalAlgo.toFixed(2)} />
         </div>
         <div className="right">
           <GovernancePeriod governanceDateTime={governanceDateTime} setShowModal={setShowModal} governancePeriod={governancePeriod} />
@@ -125,7 +123,6 @@ function App() {
       </div>
 
       <GovernancePeriodModal governancePeriod={governancePeriod} setGovernancePeriod={setGovernancePeriod} showModal={showModal} setShowModal={setShowModal} />
-
     </div>
   );
 }
--- src/components/GovernancePeriodModal.js
+++ src/components/GovernancePeriodModal.js
@@ -1,41 +1,42 @@
-import '../App.sass';
+import "../App.sass";
 import { FiCheckCircle, FiMinus } from "react-icons/fi";
 
 const ChoosePeriod = ({ title, selectPeriod, governancePeriod, id }) => {
   return (
     <button className={governancePeriod === id ? "period active" : "period"} onClick={selectPeriod} id={id}>
       {title}
-      {governancePeriod === id ? <FiCheckCircle className='checkIcon' id={id} /> : null}
+      {governancePeriod === id ? <FiCheckCircle className="checkIcon" id={id} /> : null}
     </button>
-  )
-}
+  );
+};
 
 const GovernancePeriodModal = ({ governancePeriod, setGovernancePeriod, showModal, setShowModal }) => {
   const selectPeriod = (e) => {
-    setGovernancePeriod(Number(e.target.id))
-  }
+    setGovernancePeriod(Number(e.target.id));
+  };
 
   const closeModal = () => {
-    setShowModal(false)
-  }
+    setShowModal(false);
+  };
 
   return (
     <div className={showModal ? "modal showModal" : "modal"}>
-
       <div className="top">
         <h3 className="title">Choose Period</h3>
-        <button className="closeButton" onClick={closeModal}><FiMinus /></button>
+        <button className="closeButton" onClick={closeModal}>
+          <FiMinus />
+        </button>
       </div>
 
       <div className="bottom">
+        <ChoosePeriod title={"Governance period #5"} selectPeriod={selectPeriod} id={5} governancePeriod={governancePeriod} />
         <ChoosePeriod title={"Governance period #4"} selectPeriod={selectPeriod} id={4} governancePeriod={governancePeriod} />
         <ChoosePeriod title={"Governance period #3"} selectPeriod={selectPeriod} id={3} governancePeriod={governancePeriod} />
         <ChoosePeriod title={"Governance period #2"} selectPeriod={selectPeriod} id={2} governancePeriod={governancePeriod} />
         <ChoosePeriod title={"Governance period #1"} selectPeriod={selectPeriod} id={1} governancePeriod={governancePeriod} />
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default GovernancePeriodModal
+export default GovernancePeriodModal;
